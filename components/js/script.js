39886aa633ec9e30a42c1233b865585b736d98c5
Anthony <anthonytguilherme@hotmail.com>
2024-05-09 18:15:15 -0300

    add a new blog section and responsive art

--- components/js/script.js
+++ components/js/script.js
@@ -2,13 +2,13 @@ const bar = document.getElementById("bar");
 const closeBar = document.getElementById("close");
 const nav = document.getElementById("navbar");
 
-if (bar){
-  bar.addEventListener('click', ()=> {
+if (bar) {
+  bar.addEventListener('click', () => {
     nav.classList.add('active');
   });
 }
-if (closeBar){
-  closeBar.addEventListener('click', ()=>{
+if (closeBar) {
+  closeBar.addEventListener('click', () => {
     nav.classList.remove('active');
   })
 }
